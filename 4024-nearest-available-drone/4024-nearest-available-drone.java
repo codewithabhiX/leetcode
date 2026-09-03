0d468a0f24805617bb7e2748d696c4cb2cbf411e
abhishek pandey <abhishekakp2001@gmail.com>
2026-09-04 01:08:19 +0530

    Time: 1 ms (100%), Space: 46.4 MB (55.96%) - LeetHub

--- 4024-nearest-available-drone/4024-nearest-available-drone.java
+++ 4024-nearest-available-drone/4024-nearest-available-drone.java
@@ -6,7 +6,7 @@ class Solution {
      
       for(int i=0;i<drones.length;i++){
             int temp=(Math.abs(drones[i][0]-target[0]) + Math.abs(drones[i][1]-target[1]));
-        System.out.println(temp);
+        
             if(temp<=drones[i][2]){
                 if(minrange>temp){
                     minrange=temp;
@@ -16,9 +16,7 @@ class Solution {
                 }
             }
       }  
-      if(minindex==1000){
-        return -1;
-      }
+      
       return minindex;
     }
 }
